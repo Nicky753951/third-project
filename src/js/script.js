895d3713ec571f33ce67fb6fded6a137ba792752
Nick <sytnyk.m@ukr.net>
2021-03-07 14:21:39 +0200

    learn js

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+// let num = 49;
+
+// if (num<50){
+// console.log("ok")
+// }
+
+// else {
+// console.log ("no")
+// }
+
+
+// for (let i = 1; i < 8; i++) {
+// 	console.log(i);
+// }
+
+
+// function logging(a, b) {
+// 	console.log (a+b)
+// }
+
+// logging (3,6);
+
+// let answer = prompt("ok", '');
+// {
+//	console.log(answer);
+// } 
