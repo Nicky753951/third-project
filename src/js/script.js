59aac04aff17360affbf2c3d2f9d9899ba901f52
Nick <sytnyk.m@ukr.net>
2021-03-08 12:50:35 +0200

    with errors

--- src/js/script.js
+++ src/js/script.js
@@ -24,3 +24,40 @@
 // {
 //	console.log(answer);
 // } 
+
+// $(document).ready(function(){
+//   $('.carousel__inner').slick({
+//   	speed: 1200,
+//   	// adaptiveHeight: true,
+//   	prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+//   	nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+//   });
+// });
+
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//     });
+// });
+
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
