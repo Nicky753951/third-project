3b4f4888eb4e0519e60f4922cb4144ce213e996a
Nick <sytnyk.m@ukr.net>
2021-03-13 14:14:30 +0200

    with working tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// код слик-слайдера
+
 // $(document).ready(function(){
 //     $('.carousel__inner').slick({
 //         speed: 1200,
@@ -16,6 +18,7 @@
 //     });
 // });
 
+$(document).ready(function(){
 
 const slider = tns({
     container: ".carousel__inner",
@@ -23,7 +26,24 @@ const slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false
+    nav: false,
+    speed: 1500,
+
+     responsive: {
+      640: {
+        edgePadding: 0,
+        gutter: 0,
+        items: 1
+      },
+      700: {
+        gutter: 0,
+        items: 1,
+      },
+      900: {
+        items: 1
+      }
+    }
+
   });
 
 document.querySelector('.prev').addEventListener ("click", function () {
@@ -32,4 +52,29 @@ document.querySelector('.prev').addEventListener ("click", function () {
 
 document.querySelector('.next').addEventListener ("click", function () {
   slider.goTo('next');
+});
+
+
+
+
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+
 });
