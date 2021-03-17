1d112629fab519a1d1d59c7ec09e34afff972baa
Nick <sytnyk.m@ukr.net>
2021-03-17 21:28:38 +0200

    with modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,21 @@ $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
 
+
+// Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
 });
