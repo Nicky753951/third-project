b5920c1c4e5c511d775a87607eab28b477e6f288
Nick <sytnyk.m@ukr.net>
2021-03-18 20:46:04 +0200

    with mask and valid forms

--- src/js/script.js
+++ src/js/script.js
@@ -52,4 +52,37 @@ toggleSlide('.catalog-item__back');
         });
     });
 
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+3 (809) 999-99-99");
 });
